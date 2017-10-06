531e0e429d5dd4018c52f0c592ee059e1d351bde
letimome <leticia.montalvillo@ehu.es>
2017-10-06 15:14:03 +0200

    merged lastest changes from "filter by feature branch". This commit is the last with the state of alluvial + treemaps

--- src/main/resources/static/NavigationMap.js
+++ src/main/resources/static/NavigationMap.js
@@ -10,26 +10,21 @@
 			    },    
 			    nodeStructure: {
 			        text: { name: 'diff(Platform, Product Portfolio)' },
-			      //  image: 'images/mini-alluvial.png',
-					HTMLclass: 'visitedBlue',
+			        HTMLclass: 'orange',
 			        children: [
 			            {
-			                text: { name: 'diff (AirPressure, Product Portfolio)' },
-HTMLclass: 'orange',
-children: [{
-text: { },
+			                text: { name: 'diff (Feature, Product Portfolio)' },
+							children: [{
+			               	   text: { },
 			                children: [{
-			                	 text: { name: 'diff (PL.PL asset, product.PL asset)[Filter]' },
-								HTMLclass: 'white',
+			                	 text: { name: 'diff (PL asset, Variant asset)[Filter]' },
 			                } ]
-							} ]
+			                } ]
 			            },
 			            {
 			                text: { name: 'diff(Platform,Product Variant)' },
-			          //      image: 'images/mini-product-treemap.png',
 			                children: [{
 			               	 	text: { name: 'diff (PL asset, Product Variant)' },
-			            //   	 	image: 'images/mini-VP-Treemap.png',
 			               		children: [{
 			               	 		text: { name: 'diff (PL asset, Variant asset)[Filter]' },
 			               		}]
